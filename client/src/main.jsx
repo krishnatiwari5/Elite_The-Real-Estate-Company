34ffcbbc2b4c19e04e3cc012ddd1963b4a550406
Krishna Tiwari <krishnatiwari6756@gmail.com>
2024-04-23 15:30:44 +0530

    First Commit

---
+++ client/src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+     domain="http://dev-iq6y3e70pjokpq1f.us.auth0.com"
+     clientId="BPpntyaOqSTwux1nD8fU7d2R5gUgagBb"
+     authorizationParams={{
+      redirect_uri: "http://localhost:5173"
+     }}
+     audience="http://localhost:8000"
+     scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
